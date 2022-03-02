4dd8f5ac960cf0ea7e3fb164faba1cdebac1419a
NicholasJohansan <nicholasjohansan@gmail.com>
2022-03-02 23:14:50 +0800

    Refactor script.js

--- Session 2/Projects/TicTacToe/js/script.js
+++ Session 2/Projects/TicTacToe/js/script.js
@@ -1,106 +1,10 @@
-const player = function (sign) {
+const player = function(sign) {
   return {
     sign
   };
 };
 
-const gameController = (function () {
-  let playerX = player('X');
-  let playerO = player('O');
-  let currentPlayer;
-
-  const switchPlayer = function () {
-    currentPlayer = JSON.stringify(currentPlayer) === JSON.stringify(playerX) ?
-      playerO :
-      playerX ;
-  }
-
-  const getCurrentPlayer = () => currentPlayer;
-  const setCurrentPlayer = player => currentPlayer = player;
-  const defaultPlayer = playerX;
-
-  return {
-    switchPlayer,
-    getCurrentPlayer,
-    setCurrentPlayer,
-    defaultPlayer
-  };
-})();
-
-const gameBoard = (function () {
-  let boardElement = $('#board');
-  let board;
-
-  const update = function() {
-    appStorage.saveBoardState();
-    updateTurnDisplay();
-  }
-
-  const resetBoard = function() {
-    boardElement.children('.tile').toArray().forEach( tile => {
-      $(tile).children('p').text('');
-    })
-    board = board.map(row => row.map(tile => null));
-    gameController.setCurrentPlayer(gameController.defaultPlayer);
-    update();
-  }
-
-  const updateTurnDisplay = function() {
-    $('#turn-display').hide(0, function () {
-      $(this).text(`${gameController.getCurrentPlayer().sign}\'s turn`).show();
-    });
-  };
-
-  const tileClicked = function() {
-    let sign = gameController.getCurrentPlayer().sign;
-    let row = $(this).attr('data-row');
-    let col = $(this).attr('data-col');
-
-    board[row][col] = sign;
-    $(this).children('p').fadeOut(0, function () {
-      $(this).text(sign).fadeIn(200);
-    });
-
-    gameController.switchPlayer();
-    update();
-  };
-
-  // Populate board
-  const populateBoard = function() {
-    for (let row = 0; row < 3; row++) {
-      for (let col = 0; col < 3; col++) {
-        let options = board[row][col] ?
-          { 'text': board[row][col] } :
-          {};
-        $('<div>', {
-          'data-row': row,
-          'data-col': col,
-          'class': 'tile',
-          'role': 'button'
-        }).append($('<p>', options))
-          .appendTo('#board')
-          .click(tileClicked);
-      }
-    }
-  };
-
-  const init = () => {
-    populateBoard();
-    updateTurnDisplay();
-    $('#reset-button').click(resetBoard);
-  };
-
-  const getBoard = () => board;
-  const setBoard = newBoard => board = newBoard;
-  
-  return {
-    init,
-    getBoard,
-    setBoard
-  };
-})();
-
-const appStorage = (function () {
+const appStorage = (function() {
   BOARD_STATE_KEY = "boardState";
 
   const getBoardState = function() {
@@ -117,27 +21,137 @@ const appStorage = (function () {
       }
       boardState.currentPlayer = gameController.defaultPlayer;
     }
-    gameController.setCurrentPlayer(boardState.currentPlayer);
-    gameBoard.setBoard(boardState.board);
+    return boardState
   };
 
   const saveBoardState = function() {
     console.log('saving')
     let boardState = {
       currentPlayer: gameController.getCurrentPlayer(),
-      board: gameBoard.getBoard()
+      board: gameController.getBoard()
     };
     localStorage.setItem(BOARD_STATE_KEY, JSON.stringify(boardState))
   };
 
   // Init game
-  (() => {
-    getBoardState();
-    gameBoard.init();
-  })();
 
   return {
     getBoardState,
     saveBoardState
   };
+})();
+
+const gameBoard = (function() {
+  let boardElement = $('#board');
+
+  const update = function() {
+    updateTurnDisplay();
+  }
+
+  const reset = function() {
+    boardElement.children('.tile').toArray().forEach( tile => {
+      $(tile).children('p').text('');
+    })
+    update();
+  }
+
+  const updateTurnDisplay = function() {
+    $('#turn-display').hide(0, function() {
+      $(this).text(`${gameController.getCurrentPlayer().sign}\'s turn`).show();
+    });
+  };
+
+  const setTileSign = function(tile, sign) {
+    $(tile).children('p').fadeOut(0, function() {
+      $(this).text(sign).fadeIn(200);
+    });
+  };
+
+  const populate = function() {
+    let board = gameController.getBoard();
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let options = board[row][col] ?
+          { 'text': board[row][col] } :
+          {};
+        $('<div>', {
+          'data-row': row,
+          'data-col': col,
+          'class': 'tile',
+          'role': 'button'
+        }).append($('<p>', options))
+          .appendTo('#board')
+          .click(gameController.tileClicked);
+      }
+    }
+  };
+  
+  return {
+    reset,
+    update,
+    populate,
+    setTileSign
+  };
+})();
+
+const gameController = (function() {
+  let playerX = player('X');
+  let playerO = player('O');
+  const defaultPlayer = playerX; // X starts first by default
+  let currentPlayer;
+  let board;
+
+  const switchPlayer = function() {
+    currentPlayer = JSON.stringify(currentPlayer) === JSON.stringify(playerX) ?
+      playerO :
+      playerX ;
+  }
+
+  const gameUpdate = function() {
+    gameBoard.update();
+    appStorage.saveBoardState()
+  };
+
+  const gameReset = function() {
+    board = board.map(row => row.map(() => null));
+    currentPlayer = gameController.defaultPlayer;
+    gameBoard.reset();
+    gameUpdate();
+  }
+
+  const tileClicked = function() {
+    let sign = currentPlayer.sign;
+    let row = $(this).attr('data-row');
+    let col = $(this).attr('data-col');
+
+    board[row][col] = sign;
+    gameBoard.setTileSign(this, sign);
+
+    switchPlayer();
+    gameUpdate();
+  };
+
+  const init = function() {
+    let boardState = appStorage.getBoardState();
+    board = boardState.board;
+    currentPlayer = boardState.currentPlayer;
+    gameBoard.populate();
+    gameUpdate();
+    $('#reset-button').click(gameReset);
+  }
+
+  const getBoard = () => board;
+  const getCurrentPlayer = () => currentPlayer;
+
+  return {
+    start: init,
+    tileClicked,
+    defaultPlayer,
+    getBoard,
+    getCurrentPlayer
+  };
+})();
+
+(function() {
+  gameController.start();
 })();
